3c847692af2b4db4a6b94e7719b4cb117ca6f292
aamirindi <aamirindi@gmail.com>
2024-03-16 23:34:53 +0530

    updated audios.js link

--- src/static/audios.js
+++ src/static/audios.js
@@ -1,72 +1,56 @@
-import img1 from "../assets/img/zero-two-3.gif";
-import pic1 from "../assets/img/pink.jpg";
-import song1 from "../assets/song/song9.mp3";
-import img2 from "../assets/img/luffy.gif";
-import song2 from "../assets/song/song7.mp3";
-import img3 from "../assets/img/blue.gif";
-import song3 from "../assets/song/song8.mp3";
-import img4 from "../assets/img/gojo-satoru.gif";
-import song4 from "../assets/song/song14.mp3";
-import img5 from "../assets/img/sad-anime2.gif";
-import song5 from "../assets/song/song11.mp3";
-import img6 from "../assets/img/200w.gif";
-import song6 from "../assets/song/song13.mp3";
-import img7 from "../assets/img/anime-eyes.gif";
-import song7 from "../assets/song/song12.mp3";
+const pic =
+  "https://github.com/aamirindi/songs/blob/main/images/pink.jpg?raw=tru";
 
 const audios = [
   {
-    img: img1,
-    pic: pic1,
-    src: song1,
+    img: "https://github.com/aamirindi/songs/blob/main/images/zero-two-3.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song9.mp3?raw=true",
     songName: "I Wanna Be Yours",
     songDesc: "Artic Monkeys",
   },
   {
-    img: img2,
-    pic: pic1,
-    src: song2,
+    img: "https://github.com/aamirindi/songs/blob/main/images/luffy.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song7.mp3?raw=true",
     songName: "Wasted",
     songDesc: "Nightcore",
   },
   {
-    img: img3,
-    pic: pic1,
-    src: song3,
+    img: "https://github.com/aamirindi/songs/blob/main/images/blue.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song8.mp3?raw=true",
     songName: "Hawaii",
     songDesc: "Harleys",
   },
   {
-    img: img4,
-    pic: pic1,
-    src: song4,
+    img: "https://github.com/aamirindi/songs/blob/main/images/gojo-satoru.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song14.mp3?raw=true",
     songName: "Stay with me",
     songDesc: "Miki Matsubara",
   },
   {
-    img: img5,
-    pic: pic1,
-    src: song5,
+    img: "https://github.com/aamirindi/songs/blob/main/images/sad-anime2.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song11.mp3?raw=true",
     songName: "Trampoline",
     songDesc: "Shaed",
   },
   {
-    img: img6,
-    pic: pic1,
-    src: song6,
+    img: "https://github.com/aamirindi/songs/blob/main/images/200w.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song13.mp3?raw=true",
     songName: "Courtesy Call",
     songDesc: "Thousand Foot",
   },
   {
-    img: img7,
-    pic: pic1,
-    src: song7,
+    img: "https://github.com/aamirindi/songs/blob/main/images/anime-eyes.gif?raw=true",
+    pic: pic,
+    src: "https://github.com/aamirindi/songs/blob/main/songs/song12.mp3?raw=true",
     songName: "Solitude",
     songDesc: "Felsmann",
   },
 ];
 
 export default audios;
-
-// https://raw.githubusercontent.com/user/repository/branch/filename
-// https://raw.githubusercontent.com/aamirindi/songs/main/images/15011.gif
