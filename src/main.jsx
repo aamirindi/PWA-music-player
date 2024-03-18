3592fb2c68183fb9a3c9f98f5372e91ab107aa09
aamirindi <aamirindi@gmail.com>
2024-03-18 18:24:49 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+const app = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+const rootElement = ReactDOM.createRoot(root);
+rootElement.render(app);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const manifest = {
+  name: "Music-Player",
+  short_name: "PWA",
+  description: "Music player using React (PWA)",
+  icons: [
+    {
+      src: "/logo.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+    {
+      src: "/logo.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable",
+    },
+  ],
+  theme_color: "#181818",
+  background_color: "#e0cc3b",
+  display: "standalone",
+  scope: "/",
+  start_url: "/",
+  orientation: "portrait",
+};
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  plugins: [react(), VitePWA({ manifest })],
 });
